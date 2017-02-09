ae969e7b9e4d334a16f3a294b7cb0439e0c34330
SrKomodo <jmpuchito@hotmail.com>
2017-02-09 18:59:31 -0300

    Change titlebar buttons

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -21,14 +21,14 @@ $("#close-btn").click(function () {
 
 $(function(){
   $("#min-btn").html(octicons['dash'].toSVG({ "width": 32, "height": 32}));
-  $("#max-btn").html(octicons['triangle-up'].toSVG({ "width": 32, "height": 32}));
+  $("#max-btn").html(octicons['chevron-up'].toSVG({ "width": 32, "height": 32}));
   $("#close-btn").html(octicons['x'].toSVG({ "width": 32, "height": 32}));
   $("#back").html(octicons["arrow-left"].toSVG({ "width": 32, "height": 32 }));
   let window = remote.getCurrentWindow();
   window.on("maximize", () => {
-    $("#max-btn").html(octicons['triangle-down'].toSVG({ "width": 32, "height": 32}));
+    $("#max-btn").html(octicons['chevron-down'].toSVG({ "width": 32, "height": 32}));
   });
   window.on("unmaximize", () => {
-    $("#max-btn").html(octicons['triangle-up'].toSVG({ "width": 32, "height": 32}));
+    $("#max-btn").html(octicons['chevron-up'].toSVG({ "width": 32, "height": 32}));
   });
 });
